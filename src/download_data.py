a83d750f437f108db0182be050db8c447cd9122c
Mohammad Zain <mohammad.zain@cardinaltsinc.net>
2025-03-28 19:09:49 +0530

    First commit

---
+++ src/download_data.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import pandas as pd
+
+
+ticker = input("Enter the ticker: ")
+start_date = input("Enter the start date format yyyy-mm-dd: ")
+end_date = input("Enter the end date format yyyy-mm-dd: ")
+
+def download_data(ticker, start_date, end_date):
+    data = yf.download(ticker, start = start_date, end = end_date)
+
+    data.to_csv("data.csv")
+    print("Data downloaded successfully and saved to data.csv")
+
+download_data(ticker, start_date, end_date)
+
+
